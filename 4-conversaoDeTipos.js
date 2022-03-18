905b429f0f6dd5782da6b446947f80a83727d3b4
DAIANE CRISTINA CORDEIRO <67031248+daiccordeiro@users.noreply.github.com>
2022-03-18 15:23:36 -0300

    Conversão de Tipos JS

---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,11 @@
+console.log("Conversão de Tipos");
+
+console.log("ano " + 2020);
+console.log("2" + "2"); //concatenou
+console.log(parseInt("2") + parseInt("2")); //transforma em inteiro
+
+console.log("10"/ "2"); //converte implicitamente
+console.log("Ricardo" / "2"); //NaN = not a number
+console.log("7" / "2") 
+console.log(6.5);
+console.log(6,5);
