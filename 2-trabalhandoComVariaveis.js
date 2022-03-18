a5527cbeeca6a4696a9e22670c2fdea1e5f60737
DAIANE CRISTINA CORDEIRO <67031248+daiccordeiro@users.noreply.github.com>
2022-03-18 15:07:41 -0300

    Trabalhando com variáveis JS

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,7 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 29;
+const nome = "Daiane";
+//declarar a variável usando a palavra 'const'
